485093b52bfaa6de6e10d6d77f504ac1dae5430c
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2020-07-28 13:32:23 +0430

    Updates

---
+++ precache-manifest.6846ccffd82b4c4b969893e49cabd55e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dd64608982f64ef193d13afd57e79fc",
+    "url": "/HomeMade-ANews/index.html"
+  },
+  {
+    "revision": "ce70d5b88afbd54bf722",
+    "url": "/HomeMade-ANews/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "97190c67a1bb6abd4fa5",
+    "url": "/HomeMade-ANews/static/css/main.28417ed3.chunk.css"
+  },
+  {
+    "revision": "ce70d5b88afbd54bf722",
+    "url": "/HomeMade-ANews/static/js/2.701e0719.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HomeMade-ANews/static/js/2.701e0719.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97190c67a1bb6abd4fa5",
+    "url": "/HomeMade-ANews/static/js/main.a264042a.chunk.js"
+  },
+  {
+    "revision": "1f6071b0c3eb523afe3e",
+    "url": "/HomeMade-ANews/static/js/runtime-main.0fe959d7.js"
+  }
+]);
